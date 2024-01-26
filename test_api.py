ddcba2f99159ba59c377c0a46b4f09daaad9364f
MaGmYmS <magmyms@gmail.com>
2024-01-26 16:42:37 +0500

    Обновлена документация

--- test_api.py
+++ test_api.py
@@ -2,11 +2,27 @@ import base64
 import unittest
 
 import requests
+from flasgger import Swagger
+from flask import Flask
+
+app = Flask(__name__)
 
 
 class TestMyAPI(unittest.TestCase):
-    def test_predict_endpoint(self):
-        # Подготовка изображения в формате base64
+    def test_successful_predict_endpoint(self):
+        """
+         Тест успешного запроса к /predict.
+
+         ---
+         tags:
+           - Тестирование
+         responses:
+           200:
+             description: Успешный запрос к /predict
+         x-testInfo:
+           description: Это тестовая информация для успешного теста
+           value: "Тест успешно пройден"
+         """
         path_to_img = "C:\я у мамы программист\Пробуюсь на работу\Распознование уведомлений на скриншотах v.2" \
                       "\Detected_Notification-3\\test\images\\15_jpg.rf.760b27a3ac6533de7fe011666123ecd6.jpg"
         with open(path_to_img, "rb") as image_file:
@@ -24,7 +40,32 @@ class TestMyAPI(unittest.TestCase):
         # Проверка структуры JSON-ответа
         result_json = response.json()
         self.assertIn("detected_notifications", result_json)
+        # Дополнительные проверки успешного сценария, если необходимо
+
+    def test_unsuccessful_predict_endpoint(self):
+        """
+        Тест неуспешного запроса к /predict.
+
+        ---
+        tags:
+          - Тестирование
+        responses:
+          400:
+            description: Некорректный запрос к /predict
+        x-testInfo:
+          description: Это тестовая информация для неуспешного теста
+          value: "Тест завершился неудачей"
+        """
+        # Формирование JSON-запроса
+        data = {"screenshot": "incorrect_string"}
+
+        # Отправка POST-запроса на API
+        response = requests.post("http://127.0.0.1:5000/predict", json=data)
+
+        result_json = response.json()
+        self.assertNotIn("detected_notifications", result_json)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    Swagger(app)  # Подключение Swagger для документации
     unittest.main()
